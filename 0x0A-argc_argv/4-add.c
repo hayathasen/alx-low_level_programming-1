ac9227b50ba32ea653a981b5092f3afcbd173a58
Hayat Hasen <117975937+hayathasen@users.noreply.github.com>
2023-01-06 22:33:06 +0300

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,26 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 /**
- * main - prints the number the arguments
- * @argv: array of strings of arguments
- * @argc: size of the argv
- * Return: always 0
+ * main - program that adds positive numbers
+ * @argc: this is the argument count
+ * @argv: this is the argument vector
+ *
+ * Return: 0;
  */
 int main(int argc, char *argv[])
 {
-	int i, j, sum = 0;
+	int sum = 0, i;
 
-	for (i = 1; i < argc; i++)
+	if (argc > 1)
 	{
-		for (j = 0; argv[i][j]; j++)
+		for (i = 1; i < argc; i++)
 		{
-			if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+			int b;
+			char *str;
+			
+			str = argv[i];
+			for (b = 0; str[b] != '\0'; b++
 			{
-				printf("Error\n");
-				return (1);
-			}
-		}
+				if (str[b] < 48 || str[b] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+		        }			   
+	        }			
+      }			
+      for (i = 1; i < argc; i++)
+      {
 		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
